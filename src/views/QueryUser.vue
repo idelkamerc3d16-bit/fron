4b0ffcce7cc71d84935ec03bd4a33214266f6845
Idelka Checo <idelkamerc3d16@gmail.com>
2025-10-21 10:59:58 -0400

    vamos

--- src/views/QueryUser.vue
+++ src/views/QueryUser.vue
@@ -1,12 +1,4 @@
 <script setup>
-import { ref, onMounted } from 'vue';
-import { UserService } from '@/services/UserService';
-const API_URL ='https://back-3-5d8a.onrender.com' 
-
-const users = ref([]);
-const loading = ref(true);
-const error = ref(null);
-<script setup>
 import { ref, onMounted } from 'vue'
 import { UserService } from '@/services/UserService'
 
@@ -20,7 +12,6 @@ const fetchUsers = async () => {
   loading.value = true
   error.value = null
   try {
-    // Si tu servicio no usa la URL directamente, puedes pasarla como parámetro:
     users.value = await UserService.getUsers(API_URL)
   } catch (err) {
     console.error(err)
@@ -30,7 +21,6 @@ const fetchUsers = async () => {
   }
 }
 
-// Cargar los usuarios al montar el componente
 onMounted(fetchUsers)
 </script>
 
@@ -103,47 +93,3 @@ th, td {
   text-align: left;
 }
 </style>
-
-const fetchUsers = async () => {
-  loading.value = true;
-  error.value = null;
-  try {
-    users.value = await UserService.getUsers();
-  } catch (err) {
-    error.value = 'No se pudieron cargar los usuarios. Asegúrate de que el backend esté corriendo en http://localhost:3001';
-  } finally {
-    loading.value = false;
-  }
-};
-
-// Cargar los usuarios al montar el componente
-onMounted(fetchUsers);
-</script>
-
-<template>
-  <div class="container">
-    <h2>Lista de Usuarios Registrados</h2>
-    <button @click="fetchUsers">Actualizar Lista</button>
-
-    <p v-if="loading">Cargando usuarios...</p>
-    <p v-if="error" class="error">{{ error }}</p>
-
-    <table v-if="users.length && !loading">
-      <thead>
-        <tr>
-          <th>Usuario (Username)</th>
-          <th>Nombre</th>
-          <th>Apellido</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="(user, index) in users" :key="index">
-          <td>{{ user.usuario }}</td> 
-          <td>{{ user.nombre }}</td> 
-          <td>{{ user.apellido }}</td> 
-        </tr>
-      </tbody>
-    </table>
-    <p v-if="!users.length && !loading && !error">No hay usuarios registrados.</p>
-  </div>
-</template>
